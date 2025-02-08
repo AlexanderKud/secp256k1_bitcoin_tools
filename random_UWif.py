59763b1b945317fd06ae32e5f93795bc92835d40
AlexCurl <144244712+AlexanderKud@users.noreply.github.com>
2025-02-08 05:30:22 +0300

    Update random_UWif.py

--- random_UWif.py
+++ random_UWif.py
@@ -3,7 +3,7 @@ import base58       # python3 -m pip install base58
 import binascii
 from bit import Key # python3 -m pip install bit
 import random
-import string_utils # python3 -m pip install string-utils
+import string_utils # python3 -m pip install python-string-utils
 
 #123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz - Base58
 #5H 5J 5K
